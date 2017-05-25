802758a22c2731413e01a0cafe5c537b58a4938b
Abylay Ospan <aospan@netup.ru>
2017-05-25 22:21:50 +0300

    compilation fix for linux
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- linux/include/linux/types.h
+++ linux/include/linux/types.h
@@ -192,6 +192,28 @@ static __always_inline int fls(int x)
     }                                                       \
     )
 
+
+#define __const_hweight8(w)             \
+        ((unsigned int)                 \
+         ((!!((w) & (1ULL << 0))) +     \
+          (!!((w) & (1ULL << 1))) +     \
+          (!!((w) & (1ULL << 2))) +     \
+          (!!((w) & (1ULL << 3))) +     \
+          (!!((w) & (1ULL << 4))) +     \
+          (!!((w) & (1ULL << 5))) +     \
+          (!!((w) & (1ULL << 6))) +     \
+          (!!((w) & (1ULL << 7)))))
+
+#define __const_hweight16(w) (__const_hweight8(w)  + __const_hweight8((w)  >> 8 ))
+#define __const_hweight32(w) (__const_hweight16(w) + __const_hweight16((w) >> 16))
+#define __const_hweight64(w) (__const_hweight32(w) + __const_hweight32((w) >> 32))
+
+#define hweight8(w)   __const_hweight8(w)
+#define hweight16(w)  __const_hweight16(w)
+#define hweight32(w)  __const_hweight32(w)
+#define hweight64(w)  __const_hweight64(w)
+
+
 union ktime {
           s64     tv64;
 };
