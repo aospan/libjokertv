fb95e168cac4d419691e04098abb201529b37118
Abylay Ospan <aospan@jokersys.com>
2017-10-06 11:40:10 -0700

    win build fix
    Signed-off-by: Abylay Ospan <aospan@jokersys.com>

---
+++ include/win/sys/poll.h
@@ -0,0 +1,26 @@
+/* fake poll.h
+ * for Joker TV under Windows OS
+ */
+
+#ifndef _SYS_POLL_H
+#define _SYS_POLL_H     1
+
+#define POLLIN          0x0001
+#define POLLPRI         0x0002
+#define POLLOUT         0x0004
+#define POLLERR         0x0008
+#define POLLHUP         0x0010
+#define POLLNVAL        0x0020
+
+typedef unsigned long int nfds_t;
+typedef char *caddr_t;
+
+struct pollfd {
+	int fd;
+	short events;
+	short revents;
+};
+
+#define _NO_POLL 1
+
+#endif
