232e61034a700dd123d3730b86321671bdf6f47e
Abylay Ospan <aospan@jokersys.com>
2018-03-15 10:31:11 -0400

    windows build fix (gmtime_r)
    Signed-off-by: Abylay Ospan <aospan@jokersys.com>

--- include/win/wtime.h
+++ include/win/wtime.h
@@ -11,7 +11,7 @@
 // On Windows, Visual Studio does not define gmtime_r. However, mingw might
 // do (or might not do). See https://github.com/mayah/tinytoml/issues/25,
 #ifndef gmtime_r
-static struct tm* gmtime_r(const time_t* t, struct tm* r)
+struct tm* gmtime_r(const time_t* t, struct tm* r)
 {
 	// gmtime is threadsafe in windows because it uses TLS
 	struct tm *theTm = gmtime(t);
